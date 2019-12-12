7088689e5be69d478929ca87132933be46c7c61c
Jakub <sev.jakub@gmail.com>
2019-12-12 18:17:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 const app = express();
 const staticPath = express.static(path.join(__dirname, "client/build"));
-
+const port = process.env.PORT || 3000;
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
 app.use(staticPath);
 app.use("*", staticPath);
-app.listen(3000);
+app.listen(port);
